ec63626ffbd16ed14f12f7ccc0c4551d90798b5d
EmeraldMike <nekaoumike@gmail.com>
2023-07-13 23:02:46 -0400

    add base search logic

--- utilities/search.c
+++ utilities/search.c
@@ -12,9 +12,70 @@ char filepath[100];
     if (file == NULL) {
         printf("Error: File '%s' does not exist.\n", filepath);
         return;
-    }else{
-        printf("File exists! Scanning for Val.\n");
-        
     }
-return;
+        printf("File exists! Scanning for Val.\n");
+        fseek(file, 0, SEEK_END);
+    long size = ftell(file);
+    if (size == 0) {
+        printf("Error: File '%s' is empty.\n", filepath);
+        return;
+    }
+
+    // File is not empty, read the existing JSON data
+        fseek(file, 0, SEEK_SET);
+        char *fileContent = (char *)malloc(size + 1);
+        fread(fileContent, size, 1, file);
+        fileContent[size] = '\0';
+
+        // Parse the existing JSON data
+        cJSON *json = cJSON_Parse(fileContent);
+        free(fileContent);
+        printf("File parsed!\n");
+        // check schema for key
+        cJSON *schema = cJSON_GetObjectItemCaseSensitive(json, "schema");
+        if (schema == NULL) {
+            printf("Error: Schema not found.\n");
+            return;
+        }
+        printf("%s\n", cJSON_Print(schema));
+        // check schema array for key
+        cJSON *schemaItem = NULL;
+        cJSON_ArrayForEach(schemaItem, schema) {
+            printf("Schema item: %s\n", cJSON_Print(schemaItem));
+            cJSON *keyItem = cJSON_GetObjectItemCaseSensitive(schemaItem, "key");
+            printf("Key: %s\n", cJSON_Print(keyItem));
+            if (keyItem == NULL) {
+                printf("Error: Key not found in schema.\n");
+                return;
+            }
+            // check if key matches the key we are looking for
+            printf("Key: %s\n", keyItem->valuestring);
+            if( strcmp(keyItem->valuestring, key) == 0) {
+                printf("Key found in schema!\n");
+                break;
+            }
+        }
+
+        printf("Passed schema check!\n");
+        // loop through the rows array
+        cJSON *rows = cJSON_GetObjectItemCaseSensitive(json, "rows");
+        if (rows == NULL) {
+            printf("Error: Rows not found.\n");
+            return;
+        }
+        cJSON *rowItem = NULL;
+        cJSON_ArrayForEach(rowItem, rows) {
+            cJSON *valItem = cJSON_GetObjectItemCaseSensitive(rowItem, key);
+            if (valItem == NULL) {
+                printf("Error: Key not found in row.\n");
+                return;
+            }
+            // check if key matches the key we are looking for
+            if( strcmp(valItem->valuestring, val) == 0) {
+                printf("Val found in row!\n");
+                // print the row
+                char *result = cJSON_Print(rowItem);
+                break;
+            }
+        }
 }
